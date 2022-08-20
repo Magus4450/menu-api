eb2777d8c36a73e3a5ab1e284fa906b04de75a41
Sugam Karki <sugamkarki4450@gmail.com>
2022-08-20 15:19:19 +0545

    Delete request now changes status to DELETED instead of deleting. Get list request returns only items with status either ACTIVE or PENDING.

--- api/models.py
+++ api/models.py
@@ -10,7 +10,7 @@ class Restaurant(models.Model):
     slug = models.SlugField(max_length=225, null=False, blank=False)
     location = models.CharField(max_length=225, null=False, blank=False)
     imageUrl = models.TextField(default= DEFAULT_IMG, null=False, blank=False) 
-    status = models.CharField(max_length=7, choices=OPTIONS)
+    status = models.CharField(max_length=7, choices=OPTIONS, default=("PENDING", "PENDING"))
 
     def __str__(self):
         return self.name
@@ -21,7 +21,7 @@ class Category(models.Model):
     slug = models.SlugField(max_length=225, null=False, blank=False)
     restaurantId = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     imageUrl = models.TextField(default= DEFAULT_IMG, null=False, blank=False) 
-    status = models.CharField(max_length=7, choices=OPTIONS)
+    status = models.CharField(max_length=7, choices=OPTIONS, default=("PENDING", "PENDING"))
 
 
     def __str__(self):
@@ -35,7 +35,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
     description = models.TextField(default= DEFAULT_IMG, max_length=225, null=False, blank=False)
     imageUrl = models.TextField(null=False, blank=False)
-    status = models.CharField(max_length=7, choices=OPTIONS)
+    status = models.CharField(max_length=7, choices=OPTIONS, default=("PENDING", "PENDING"))
 
     def __str__(self):
         return self.name
--- api/views.py
+++ api/views.py
@@ -1,28 +1,49 @@
 from . import models
-
+from django.db.models import Q
 from rest_framework import generics
 from . import serializers
 
 class RestaurantListCreateAPIView(generics.ListCreateAPIView):
-    queryset = models.Restaurant.objects.all()
+    # Filter by status
+    queryset = models.Restaurant.objects.filter(Q(status="ACTIVE") | Q(status="PENDING"))
     serializer_class = serializers.RestaurantSerializer
+    
 
 
 class RestaurantRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Restaurant.objects.all()
     serializer_class = serializers.RestaurantSerializer
 
+    def destroy(self):
+        instance = self.get_object()
+        instance.status = "DELETED"
+        instance.save()
+        data = {
+            "status": "success",
+        }
+        return Response(data, status=status.HTTP_200_OK)
+        
+
 class CategoryListCreateAPIView(generics.ListCreateAPIView):
-    queryset = models.Category.objects.all()
+    queryset = models.Category.objects.filter(Q(status="ACTIVE") | Q(status="PENDING"))
     serializer_class = serializers.CategorySerializer
 
 class CategoryRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Category.objects.all()
     serializer_class = serializers.CategorySerializer
 
+    def destroy(self):
+        instance = self.get_object()
+        instance.status = "DELETED"
+        instance.save()
+        data = {
+            "status": "success",
+        }
+        return Response(data, status=status.HTTP_200_OK)
+
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
-    queryset = models.Product.objects.all()
+    queryset = models.Product.objects.filter(Q(status="ACTIVE") | Q(status="PENDING"))
     serializer_class = serializers.ProductSerializer
 
 
@@ -31,6 +52,15 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Product.objects.all()
     serializer_class = serializers.ProductSerializer
+
+    def destroy(self):
+        instance = self.get_object()
+        instance.status = "DELETED"
+        instance.save()
+        data = {
+            "status": "success",
+        }
+        return Response(data, status=status.HTTP_200_OK)
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -49,6 +79,7 @@ def upload_image(request):
 
     import os
     ROOT_URL = settings.MEDIA_ROOT
+    print(ROOT_URL)
     FINAL_URL = os.path.join(ROOT_URL, type)
 
     if not os.path.exists(FINAL_URL):
@@ -58,7 +89,7 @@ def upload_image(request):
     im = Image.open(image.temporary_file_path())
     
     from django.utils import timezone
-
+    
     time = str(timezone.now().strftime("%Y%m%d%H%M%S"))
     split = image.name.split(".")
     name = "".join(split[:-1])
@@ -69,11 +100,13 @@ def upload_image(request):
 
 
     im = im.save(IMG_URL)
-
+    print(ROOT_URL.replace("\\media",""))
+    print(IMG_URL)
     imageUrl = IMG_URL.replace(ROOT_URL.replace("\\media",""),"")
     data = {
         "imageUrl": imageUrl
     }
+    print(imageUrl)
     return Response(data, status=status.HTTP_200_OK)
 
 
